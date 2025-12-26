6e2c0f0fe323c681c0d824530ce5e55af0f7240e
Olarewaju Mojeed Olalekan <olarewajumojeed9@gmail.com>
2025-12-26 10:13:53 +0100

    Send notification on license key generation

---
+++ app/Console/Commands/ConfigureApp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ConfigureApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'configure-app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set up the command for use';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
