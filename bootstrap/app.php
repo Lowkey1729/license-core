1b4db85974eb6983ca99e13d9cc276051d657f62
Olarewaju Mojeed Olalekan <olarewajumojeed9@gmail.com>
2025-12-28 11:40:39 +0100

    robust error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,15 +9,16 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -61,10 +62,20 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
 
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            if ($request->wantsJson()) {
+                return new FailureResponse(
+                    message: $e->getMessage(),
+                    httpStatusCode: 422
+                );
+            }
+        });
+
         $exceptions->render(function (Throwable $e, Request $request) {
 
             if (! $request->expectsJson()) {
                 Log::error($e);
+
                 return null;
             }
 
@@ -74,6 +85,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
             if ($statusCode >= 500) {
                 Log::error($e);
+
                 return new FailureResponse(
                     message: 'An unexpected error occurred. Please try again later.',
                     httpStatusCode: 500
